f77da0e10dc2dea6b88b8c79287401468a43e812
Ridha Ilham Adi Setyawan <117886100+ridhailham@users.noreply.github.com>
2023-07-12 21:40:47 +0700

    update-3

--- src/components/Sections/Hero.jsx
+++ src/components/Sections/Hero.jsx
@@ -1,4 +1,4 @@
-// import React from "react";
+import React from "react";
 
 const Hero = () => {
   return (
@@ -12,9 +12,9 @@ const Hero = () => {
       }}
     >
       <div className="min-w-full min-h-[100vh] flex flex-col items-center justify-center gap-8">
-        <div className="bg-white w-[60vw] h-[50vh] flex gap-8 flex-col justify-center items-center rounded-[100px] shadow-2xl shadow-[#ff4800] border: to-black">
+        <div className="bg-white w-[60vw] h-[50vh] flex gap-8 flex-col justify-center items-center rounded-[100px] shadow-2xl shadow-[#ff4800] border: to-black" style={styles.card}>
           <h3 className="bg-gradient-to-br bg-clip-text from-[#F34E4E] text-transparent to-[#ffdd00] text-4xl md:text-9xl font-bold text-center">Belly</h3>
-          <h3 className=" bg-gradient-to-br bg-clip-text from-[#F34E4E] text-transparent to-[#fec7a0]  font-bold text-[20px] md:text-3xl">
+          <h3 className="bg-gradient-to-br bg-clip-text from-[#F34E4E] text-transparent to-[#fec7a0] font-bold text-[20px] md:text-3xl">
             Choose With Your Pleasure
           </h3>
         </div>
@@ -24,3 +24,29 @@ const Hero = () => {
 };
 
 export default Hero;
+
+const styles = {
+  card: {
+    animation: "customMoveAnimation 3s infinite",
+  },
+};
+
+// CSS di bawah ini dapat ditambahkan di dalam file Hero.js
+
+const css = `
+@keyframes customMoveAnimation {
+  0% {
+    transform: translateX(-50px);
+  }
+  50% {
+    transform: translateX(50px);
+  }
+  100% {
+    transform: translateX(-50px);
+  }
+}
+`;
+
+const styleTag = document.createElement("style");
+styleTag.innerHTML = css;
+document.head.appendChild(styleTag);
