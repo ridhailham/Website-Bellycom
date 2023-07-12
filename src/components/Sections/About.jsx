e08ba587a299cf07c7c2c0b1c4ef4eba6736a616
Ridha Ilham Adi Setyawan <117886100+ridhailham@users.noreply.github.com>
2023-07-12 23:12:46 +0700

    updatee-about

--- src/components/Sections/About.jsx
+++ src/components/Sections/About.jsx
@@ -5,13 +5,12 @@ const About = () => {
     <div className="bg-[#ff4800] h-[70vh] w-[100vw] flex justify-center py-5 gap-8">
       <div className="flex flex-col gap-8">
         <div className="min-w-full min-h-[30vh] flex flex-col items-center justify-center gap-8">
-          <div className="bg-white w-[60vw] h-[18vh] flex gap-8 flex-col justify-center items-center rounded-[10px] shadow-2xl shadow-[#000000]">
-            <h3 className="bg-gradient-to-br bg-clip-text from-[#F34E4E] text-transparent to-[#ffdd00] text-4xl md:text-7xl font-bold text-center ">About Us</h3>
-
+          <div className="bg-white w-full md:w-[60vw] h-[18vh] flex gap-8 flex-col justify-center items-center rounded-[10px] shadow-2xl shadow-[#000000]">
+            <h3 className="bg-gradient-to-br bg-clip-text from-[#F34E4E] text-transparent to-[#ffdd00] text-4xl md:text-3xl font-bold text-center">About Us</h3>
           </div>
         </div>
-        <h3 className="text-3xl font-medium">BELLY.COM is a business entity that focuses on developing sales in the field of food and beverages whose target market is Brawijaya University residents.
-          Belly.com is a food and beverage business that focuses on unique, delicious and nutritious flavors. In addition to prices that are suitable for students, now belly.com presents several menus that can follow current market trends.
+        <h3 className="text-3xl md:text-xl font-medium">
+          BELLY.COM is a business entity that focuses on developing sales in the field of food and beverages whose target market is Brawijaya University residents. Belly.com is a food and beverage business that focuses on unique, delicious, and nutritious flavors. In addition to prices that are suitable for students, now belly.com presents several menus that can follow current market trends.
         </h3>
       </div>
     </div>
